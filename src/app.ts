72ff4310f38e5bc3e75fdd9cea381120cc96008e
noob-ubaid <ubaidurrahman661@gmail.com>
2026-01-02 23:09:19 +0600

    add cors

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,15 @@ import express, { Application } from "express";
 import { postRouter } from "./modules/post/post.router";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
+import cors from 'cors'
+
 const app: Application = express();
+
+
+app.use(cors({
+  origin : process.env.APP_URL,
+  credentials : true
+}))
 app.use(express.json());
 app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use("/posts", postRouter);
