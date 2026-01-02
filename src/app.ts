3f4a9559d6cd4f6150f5ec7b3b205c7597ac9f12
noob-ubaid <ubaidurrahman661@gmail.com>
2026-01-02 22:44:54 +0600

    add additional information

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Application } from "express";
 import { postRouter } from "./modules/post/post.router";
-
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 const app: Application = express();
 app.use(express.json());
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use("/posts", postRouter);
 app.use("/", async (req, res) => {
   res.send("Hello World");
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,26 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
-    }),
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "USER",
+      },
+      phone: {
+        type: "string",
+        required: false,
+      },
+      status: {
+        type: "string",
+        required: false,
+      },
+    },
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
 });
