700705e0ae5d35c05e646d25fbe6fd16e432fb74
noob-ubaid <ubaidurrahman661@gmail.com>
2026-01-30 15:10:00 +0600

    solve notFOund functionality

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { auth } from "./lib/auth";
 import cors from "cors";
 import { commentRouter } from "./modules/comment/comment.router";
 import errorHandler from "./middleware/globalErrorHandler";
+import { notFound } from "./middleware/notFound";
 
 const app: Application = express();
 
@@ -12,14 +13,16 @@ app.use(
   cors({
     origin: process.env.APP_URL,
     credentials: true,
-  })
+  }),
 );
 app.use(express.json());
-app.all("/api/auth/*splat", toNodeHandler(auth));
-app.use("/posts",  postRouter);
-app.use("/comments",  commentRouter);
-app.use("/", async (req, res) => {
+app.get("/", (req, res) => {
   res.send("Welcome to blog app server");
 });
-app.use(errorHandler)
+app.all("/api/auth/*splat", toNodeHandler(auth));
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 export default app;
--- src/middleware/notFound.ts
+++ src/middleware/notFound.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 
 export function notFound(req: Request, res: Response) {
-    res.status(404).json({
-        message: "Route not found!",
-        path: req.originalUrl,
-        date: Date()
-    })
+  res.status(404).json({
+    message: "Route not found!",
+    path: req.originalUrl,
+    date: Date(),
+  });
 }
